160efce022f7647cca67a1144408b46c39ec2edc
Nate Harms <nathan.d.harms@gmail.com>
2025-09-11 13:01:17 -0400

    adding some more

---
+++ src/pipeline/__version__.py
@@ -0,0 +1,14 @@
+"""
+__version__.py
+~~~~~~~~~~~~~~
+
+Information about the current version of the py-package-template package.
+"""
+
+__title__ = 'peptide-agent'
+__description__ = ''
+__version__ = '0.0.0'
+__author__ = ''
+__author_email__ = ''
+__license__ = 'MIT'
+__url__ = ''
---
+++ src/pipeline/main.py
@@ -0,0 +1,9 @@
+import typer
+
+
+def main():
+    ...
+
+
+if __name__ == "__main__":
+    main()
