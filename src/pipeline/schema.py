dcb950019cd367e346afc9d2a6ffd4eb7aa0342e
Nate Harms <nathan.d.harms@gmail.com>
2025-09-11 21:28:51 -0400

    making explicit categories

--- src/pipeline/schema.py
+++ src/pipeline/schema.py
@@ -4,8 +4,22 @@ peptide_output_schema = Schema(
     {
         "PEPTIDE_CODE": str,
         "MORPHOLOGY": Or("None", "Fiber", "Sphere", "Aggregate"),
-        "PH": int,
-        "CONCENTRATION_LOG_M": int,
+        "PH": Or(
+            "(1,3)",
+            "(3,5)",
+            "(5,6.5)",
+            "(6.5,7.5)",
+            "(7.5,9)",
+            "(9,11)",
+            "(11,14)"
+        ),
+        "CONCENTRATION_LOG_MGML": [
+            "(-3,-1)",
+            "(-1,0)",
+            "(0,1)",
+            "(1,2)",
+            "(2,3)",
+        ],
         "TEMPERATURE_C": Or("37", "25"),
         "SOLVENT": Or(
             "Dimethylformamide (DMF)",
